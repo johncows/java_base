b99dad02dc4c0967902fcf9bc7a8b1c6454213b7
王骏康 <junkang.wang@guanshantech.com>
2020-12-17 19:55:19 +0800

    java8 dateTime study

--- 2020/java8-learn/src/main/java/com/kk/cheapter11/LocalDateTest.java
+++ 2020/java8-learn/src/main/java/com/kk/cheapter11/LocalDateTest.java
@@ -2,10 +2,15 @@ package com.kk.cheapter11;
 
 import org.junit.Test;
 
+import java.text.SimpleDateFormat;
 import java.time.*;
 import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoField;
+import java.util.Date;
 import java.util.Optional;
 import java.util.OptionalInt;
+import java.util.OptionalLong;
+import java.util.stream.IntStream;
 
 /**
  * @author wangjunkang
@@ -15,73 +20,125 @@ import java.util.OptionalInt;
  */
 public class LocalDateTest {
 
-    @Test
-    public void fun1() {
-        // 线程安全
-        Optional.of(LocalDate.now()).ifPresent(System.out::println);
 
+    /**
+     * @Description: jdk 1.8前的date存在以下问题
+     * 1. date 语意是日期，但查询出来 日期+时间;存在含义不清晰；搭配的api较少
+     * 2. dateformat 常用的格式化类有可能存在线程安全异常
+     * @Param: 
+     * @return: 
+     * @Author: wjk
+     * @Date: 2020/12/17
+     */     
+    @Test
+    public void badExample() throws InterruptedException {
+        SimpleDateFormat sdf = new SimpleDateFormat("YYYY-mm-dd");
+
+
+        IntStream.range(0, 10).forEach(i ->
+                new Thread(() -> {
+                    for (; ; ) {
+                        String format = sdf.format(new Date());
+                        System.out.println(Thread.currentThread() + format);
+                    }
+                }).start()
+        );
+
+
+        Thread.sleep(1000_000);
+
+
+    }
+
+
+    @Test
+    public void testLocalDate() {
+        Optional.of(LocalDate.now()).ifPresent(System.out::println);
         Optional.of(LocalDate.now().getYear()).ifPresent(System.out::println);
         Optional.of(LocalDate.now().getMonth()).ifPresent(System.out::println);
         Optional.of(LocalDate.now().getDayOfMonth()).ifPresent(System.out::println);
         Optional.of(LocalDate.now().getChronology()).ifPresent(System.out::println);
         Optional.of(LocalDate.now().getYear()).ifPresent(System.out::println);
-
     }
 
-     @Test
-     public  void fun2(){
-         OptionalInt.of(LocalTime.now().getHour()).ifPresent(System.out::println);
-         Optional.of(LocalDateTime.of(LocalDate.now(), LocalTime.now())).ifPresent(System.out::println);
-     }
+    @Test
+    public void testLocalTime() {
+        OptionalInt.of(LocalTime.now().getHour()).ifPresent(System.out::println);
+        OptionalInt.of(LocalTime.now().getMinute()).ifPresent(System.out::println);
+        OptionalInt.of(LocalTime.now().getSecond()).ifPresent(System.out::println);
+    }
 
 
-    // 传统的date方式的弊端及sdf的线程错误 mutipo point
+    @Test
+    public void testLocalDateTime() {
+        OptionalInt.of(LocalDateTime.now().get(ChronoField.DAY_OF_MONTH)).ifPresent(System.out::println);
+        Optional.of(LocalDateTime.of(LocalDate.now(), LocalTime.now())).ifPresent(System.out::println);
+    }
 
 
+    /**
+     * @Description: instant 表示是某个时间点，duration 表示2个时间点的时间段
+     * @Param:
+     * @return:
+     * @Author: wjk
+     * @Date: 2020/12/17
+     */
+    @Test
+    public void testInstanceAndDuration() throws InterruptedException {
 
-     @Test
-     public  void fun3() throws InterruptedException {
-         Optional.ofNullable(Instant.now()).ifPresent(System.out::println);
-         Thread.sleep(1000L);
-         Optional.ofNullable(Instant.now()).ifPresent(System.out::println);
-     }
+        Optional.ofNullable(Instant.now()).ifPresent(System.out::println);
+        Thread.sleep(1000L);
+        Optional.ofNullable(Instant.now()).ifPresent(System.out::println);
 
-      @Test
-      public  void fun4() throws InterruptedException {
-          Instant start = Instant.now();
+        Instant start = Instant.now();
+        Thread.sleep(1000L);
+        Instant end = Instant.now();
 
-          Thread.sleep(1000L);
-
-          Instant end = Instant.now();
-
-          long l = Duration.between(start, end).toNanos();
-
-          System.out.println("l = " + l);
-      }
-
-
-       @Test
-       public  void fun5(){
-           int days = Period.ofDays(1).getDays();
-           System.out.println("days = " + days);
-       }
+        OptionalLong.of(Duration.between(start, end).getSeconds()).ifPresent(System.out::println);
+    }
 
 
-        @Test
-        public  void fun6(){
-            LocalDate localDate = LocalDate.now();
-            Optional.of(localDate.format(DateTimeFormatter.BASIC_ISO_DATE)).ifPresent(System.out::println);
-            Optional.of(localDate.format(DateTimeFormatter.ISO_LOCAL_DATE)).ifPresent(System.out::println);
-        }
+    /**
+     * @Description: Period 纪元 使用方式
+     * @Param:
+     * @return:
+     * @Author: wjk
+     * @Date: 2020/12/17
+     */
+    @Test
+    public void testPeriod() {
+        LocalDate birthday = LocalDate.of(1996, 11, 28);
+        OptionalInt.of(Period.between(birthday, LocalDate.now()).getDays()).ifPresent(System.out::println);
+    }
 
 
-         @Test
-         public  void fun7(){
-            String dateForStr = "20201128";
-             LocalDate parse = LocalDate.parse(dateForStr, DateTimeFormatter.BASIC_ISO_DATE);
-             System.out.println("parse = " + parse);
-         }
+    /**
+     * @Description: 格式化日期参数
+     * @Param:
+     * @return:
+     * @Author: wjk
+     * @Date: 2020/12/17
+     */
+    @Test
+    public void dateFormat() {
+        LocalDate localDate = LocalDate.now();
+        Optional.of(localDate.format(DateTimeFormatter.BASIC_ISO_DATE)).ifPresent(System.out::println);
+        Optional.of(localDate.format(DateTimeFormatter.ISO_LOCAL_DATE)).ifPresent(System.out::println);
+    }
 
 
+    /**
+     * @Description: 对日期参数做解析
+     * @Param:
+     * @return:
+     * @Author: wjk
+     * @Date: 2020/12/17
+     */
+    @Test
+    public void dateParse() {
+        String dateForStr = "20201128";
+        LocalDate parse = LocalDate.parse(dateForStr, DateTimeFormatter.BASIC_ISO_DATE);
+        System.out.println("parse = " + parse);
+    }
 }
 
